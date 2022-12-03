4b9ee5ce4a54b4ebd0df3a0eca9f47db0c7ee45a
nuuuwan <nuuuwan@gmail.com>
2022-12-03 13:22:17 +0530

    style: padding

--- src/view/atoms/CountryView.js
+++ src/view/atoms/CountryView.js
@@ -3,8 +3,9 @@ import { Avatar, Box } from "@mui/material";
 import Flags from "../../nonview/core/Flags";
 
 const STYLE_IMAGE = {
-  width: "4vh",
-  height: "4vh",
+  width: "3vh",
+  height: "3vh",
+  padding: 1,
 };
 
 export default function CountryView({ country }) {
