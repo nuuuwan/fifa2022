e72f5a9e3392f9974b3b12fe2a0dbf1d67b30acb
nuuuwan <nuuuwan@gmail.com>
2022-12-03 16:56:48 +0530

    refac: Made HomePage a Component

--- src/view/pages/HomePage.js
+++ src/view/pages/HomePage.js
@@ -1,3 +1,5 @@
+import { Component } from "react";
+
 import { Box } from "@mui/material";
 
 import Simulate from "../../nonview/core/Simulate";
@@ -5,24 +7,26 @@ import Simulate from "../../nonview/core/Simulate";
 import StageView from "../atoms/StageView";
 import CustomBottomNavigation from "../molecules/CustomBottomNavigation";
 
-export default function HomePage() {
-  const simulationResults = Simulate.random();
-  const renderedInner = Object.values(simulationResults).map(function (
-    gameList,
-    iStage
-  ) {
-    const key = "stage-" + iStage;
-    return <StageView key={key} gameList={gameList} />;
-  });
+export default class HomePage extends Component {
+  render() {
+    const simulationResults = Simulate.random();
+    const renderedInner = Object.values(simulationResults).map(function (
+      gameList,
+      iStage
+    ) {
+      const key = "stage-" + iStage;
+      return <StageView key={key} gameList={gameList} />;
+    });
 
-  return (
-    <Box>
-      <table>
-        <tbody>
-          <tr>{renderedInner}</tr>
-        </tbody>
-      </table>
-      <CustomBottomNavigation />
-    </Box>
-  );
+    return (
+      <Box>
+        <table>
+          <tbody>
+            <tr>{renderedInner}</tr>
+          </tbody>
+        </table>
+        <CustomBottomNavigation />
+      </Box>
+    );
+  }
 }
