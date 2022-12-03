4e63085f8ad9acb9e148b67f1082526e38544724
nuuuwan <nuuuwan@gmail.com>
2022-12-03 09:56:00 +0530

    feat: basic view

--- src/view/molecules/HomePage.js
+++ src/view/molecules/HomePage.js
@@ -1,6 +1,34 @@
 import Simulate from "../../nonview/core/Simulate";
 
+function GameView({ t1, t2 }) {
+  return (
+    <div>
+      <div>{t1}</div>
+      <div>{t2}</div>
+    </div>
+  );
+}
+
+function StageView({ gameList }) {
+  const renderedInner = gameList.map(function ([t1, t2]) {
+    return <GameView t1={t1} t2={t2} />;
+  });
+  return <td>{renderedInner}</td>;
+}
+
 export default function HomePage() {
   const simulationResults = Simulate.random();
-  return JSON.stringify(simulationResults);
+  const renderedInner = Object.values(simulationResults).map(function (
+    gameList
+  ) {
+    return <StageView gameList={gameList} />;
+  });
+
+  return (
+    <table>
+      <tbody>
+        <tr>{renderedInner}</tr>
+      </tbody>
+    </table>
+  );
 }
