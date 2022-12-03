7dd8fd3b9d7a1b9a929474d59e86dfb2e7d5a7f5
nuuuwan <nuuuwan@gmail.com>
2022-12-03 17:37:10 +0530

    feat: Added days

---
+++ src/nonview/constants/DAYS.js
@@ -0,0 +1,7 @@
+const DAYS = {
+  r16: ["Dec 3", "Dec 3", "Dec 4", "Dec 4", "Dec 5", "Dec 5", "Dec 6", "Dec 6"],
+  r8: ["Dec 9", "Dec 9", "Dec 10", "Dec 10"],
+  r4: ["Dec 13", "Dec 14"],
+  r2: ["Dec 18"],
+};
+export default DAYS;
