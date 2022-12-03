0406f81477a55215012a33491f2dc7c50e23c56f
nuuuwan <nuuuwan@gmail.com>
2022-12-03 13:16:15 +0530

    style: boxes, Avatar

--- src/view/atoms/CountryView.js
+++ src/view/atoms/CountryView.js
@@ -1,10 +1,20 @@
+import { Avatar, Box } from "@mui/material";
+
 import Flags from "../../nonview/core/Flags";
 
 const STYLE_IMAGE = {
-  width: "6.25vh",
+  width: "4vh",
+  height: "4vh",
 };
 
 export default function CountryView({ country }) {
+  if (!country) {
+    return null;
+  }
   const imageSrc = Flags.getImageSrc(country);
-  return <img alt={country} src={imageSrc} style={STYLE_IMAGE} />;
+  return (
+    <Box>
+      <Avatar src={imageSrc} style={STYLE_IMAGE} />
+    </Box>
+  );
 }
--- src/view/atoms/GameView.js
+++ src/view/atoms/GameView.js
@@ -1,8 +1,15 @@
 import CountryView from "./CountryView";
 
+const STYLE_GAME = {
+  border: "1px gray solid",
+  borderRadius: 6,
+  padding: 6,
+  margin: 6,
+};
+
 export default function GameView({ country1, country2 }) {
   return (
-    <div>
+    <div style={STYLE_GAME}>
       <div>
         <CountryView country={country1} />
       </div>
