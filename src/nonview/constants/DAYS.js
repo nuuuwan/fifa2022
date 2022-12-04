eac65be1aacada8dc79b6c0957383615c820da81
nuuuwan <nuuuwan@gmail.com>
2022-12-04 08:43:09 +0530

    feat: Added days of week

--- src/nonview/constants/DAYS.js
+++ src/nonview/constants/DAYS.js
@@ -1,7 +1,7 @@
 const DAYS = {
-  r16: ["Dec 3", "Dec 3", "Dec 4", "Dec 4", "Dec 5", "Dec 5", "Dec 6", "Dec 6"],
-  r8: ["Dec 9", "Dec 9", "Dec 10", "Dec 10"],
-  r4: ["Dec 13", "Dec 14"],
-  r2: ["Dec 18"],
+  r16: ["Dec 3 (Sat)", "Dec 3 (Sat)", "Dec 4 (Sun)", "Dec 4 (Sun)", "Dec 5 (Mon)", "Dec 5 (Mon)", "Dec 6 (Tue)", "Dec 6 (Tue)"],
+  r8: ["Dec 9 (Fri)", "Dec 9 (Fri)", "Dec 10 (Sat)", "Dec 10 (Sat)"],
+  r4: ["Dec 13 (Tue)", "Dec 14 (Wed)"],
+  r2: ["Dec 18 (Sun)"],
 };
 export default DAYS;
