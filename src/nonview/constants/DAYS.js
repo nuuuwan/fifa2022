914a48df4b1c76265daad2f4f4636fcc5ce37d71
nuuuwan <nuuuwan@gmail.com>
2022-12-04 11:49:08 +0530

    fix: More DAYS BUGS

--- src/nonview/constants/DAYS.js
+++ src/nonview/constants/DAYS.js
@@ -1,16 +1,16 @@
 const DAYS = {
   r16: [
-    "Dec 3 (Sat)",
-    "Dec 3 (Sat)",
-    "Dec 5 (Mon)",
-    "Dec 5 (Mon)",
-    "Dec 4 (Sun)",
-    "Dec 4 (Sun)",    
-    "Dec 6 (Tue)",
-    "Dec 6 (Tue)",
+    "Dec 3 (Sat), 6pm",
+    "Dec 3 (Sat), 10pm",
+    "Dec 5 (Mon), 6pm",
+    "Dec 5 (Mon), 10pm",
+    "Dec 4 (Sun), 6pm",
+    "Dec 4 (Sun), 10pm",
+    "Dec 6 (Tue), 6pm",
+    "Dec 6 (Tue), 10pm",
   ],
-  r8: ["Dec 9 (Fri)", "Dec 9 (Fri)", "Dec 10 (Sat)", "Dec 10 (Sat)"],
-  r4: ["Dec 13 (Tue)", "Dec 14 (Wed)"],
-  r2: ["Dec 18 (Sun)"],
+  r8: ["Dec 9 (Fri), 10pm", "Dec 9 (Fri), 6pm",  "Dec 10 (Sat), 10pm", "Dec 10 (Sat), 6pm"],
+  r4: ["Dec 13 (Tue), 10pm", "Dec 14 (Wed), 10pm"],
+  r2: ["Dec 18 (Sun), 6pm"],
 };
 export default DAYS;
