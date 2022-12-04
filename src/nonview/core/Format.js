a31d92c41fe8ebb98be4235605b456abdd24cf5a
nuuuwan <nuuuwan@gmail.com>
2022-12-04 11:34:52 +0530

    feat: Format fraction

--- src/nonview/core/Format.js
+++ src/nonview/core/Format.js
@@ -11,9 +11,19 @@ export default class Format {
   }
 
   static fraction(f) {
+    const MAX_DEN = 10;
+    const MAX_ABS_ERR = 0.1;
     if (f > 0.1) {
+      for (let den = 2; den < MAX_DEN; den++) {
+        const num = parseInt(f * den + 0.5);
+        const f2 = num / den;
+        const err = Math.abs(f2 - f) / f;
+        if (err < MAX_ABS_ERR && num !== den) {
+          return num + "/" + den;
+        }
+      }
       return Format.percent(f);
     }
-    return "1 in " + Format.roundLog(1 / f).toLocaleString();
+    return "1/" + Format.roundLog(1 / f).toLocaleString();
   }
 }
