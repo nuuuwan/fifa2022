20c88be3f9cec5ad3ed7848e4e4eab293496d74f
nuuuwan <nuuuwan@gmail.com>
2022-12-04 11:39:16 +0530

    fix: DAYS bug

--- src/nonview/constants/R16.js
+++ src/nonview/constants/R16.js
@@ -1,10 +1,17 @@
 const R16 = [
+  // Dec 3 (Sat)
   ["Netherlands", "USA"],
   ["Argentina", "Australia"],
-  ["Japan", "Croatia"],
-  ["Brazil", "South Korea"],
+
+  // Dec 4 (Sun)  
   ["France", "Poland"],
   ["England", "Senegal"],
+
+  // Dec 5 (Mon)    
+  ["Japan", "Croatia"],
+  ["Brazil", "South Korea"],
+  
+  // Dec 6 (Tue)    
   ["Morocco", "Spain"],
   ["Portugal", "Switzerland"],
 ];
